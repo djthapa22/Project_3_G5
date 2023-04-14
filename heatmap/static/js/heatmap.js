ae38d02295bd930a0f02844f4e8f73d6020bfc97
Tanner <tannerjvictorian@gmail.com>
2023-04-13 21:40:36 -0500

    updated

--- heatmap/static/js/heatmap.js
+++ heatmap/static/js/heatmap.js
@@ -5,7 +5,6 @@ let myMap = L.map("map", {
 
 //Adding the tile layer
 
-
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
